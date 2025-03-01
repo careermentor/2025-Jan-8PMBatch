94ff034ec675ccb49a30adf971c00accfb8bc0b5
Santosh Modi <modisantosh@gmail.com>
2025-03-01 20:45:18 +0530

    Saucedemo project updated

--- src/org/wipro/auto/saucedemo/testcase/LoginScenario.java
+++ src/org/wipro/auto/saucedemo/testcase/LoginScenario.java
@@ -12,23 +12,13 @@ public class LoginScenario extends InitiateBrowse
 	{
 		
 		LoginPage login = new LoginPage(driver);
-		login.enter_username("user1");
-		login.enter_password("pass1234");
+		login.enter_username("standard_user");
+		login.enter_password("secret_sauce");
 		login.click_loginbttn();
 		
 		//because "this.driver" is null
 	}
 	
-	@Test
-	public void tc01_validate_login_Invalid_credential() throws Exception
-	{
-		
-		LoginPage login = new LoginPage(driver);
-		login.enter_username("xyz");
-		login.enter_password("pass1234");
-		login.click_loginbttn();
-		
-		
-	}
+
 	
 }
